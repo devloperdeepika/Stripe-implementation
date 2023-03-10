2cfcc6d0653e2869e14c5d0a276fd68fe7aee651
developerdeepika <deepikadangi9060@gmail.com>
2023-03-10 12:15:05 +0530

    Stripe implementaion

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :charges, only: [:new, :create]
   resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # root "home#index"
+  root to: 'posts#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
 end
